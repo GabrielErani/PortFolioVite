c4adc4496acb6082ec71eb64a246d76dcfe9cd8d
unknown <gabrielciriloerani@gmail.com>
2024-04-08 23:54:28 -0300

    tentando hospedar no gitpages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PortFolioVite/',
   plugins: [react()],  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
