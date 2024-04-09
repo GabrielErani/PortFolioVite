62302e913d2dad0eb994c5cebfe633a9e88ea84f
unknown <gabrielciriloerani@gmail.com>
2024-04-09 00:10:55 -0300

    sla

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://GabrielErani.github.io/PortFolioVite/",
   plugins: [react()],  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
